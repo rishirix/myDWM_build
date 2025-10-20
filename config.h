0551905cb583bdcb5b6be93e6df2035541b8cf7a
Rishi <rishibhargawa47@gmail.com>
2025-10-20 10:25:56 +0530

    updated build-stable 1.0

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Liberation Mono:size=7" };
-static const char dmenufont[]       = "Liberation Mono:size=7";
+static const char *fonts[]          = { "Roboto Condensed:size=6" };
+static const char dmenufont[]       = "Roboto Condensed:size=6";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -53,8 +53,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
-#define ALTMOD Mod4Mask
+#define MODKEY Mod4Mask
+#define ALTMOD Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *upbrightness[] = {"xbacklight","-inc","10",NULL};
 static const char *downbrightness[] = {"xbacklight","-dec","10",NULL};
 static const char *volup[] = {"wpctl","set-volume","@DEFAULT_SINK@","5%+",NULL};
@@ -119,7 +119,7 @@ static const Key keys[] = {
 	{0,                             XF86XK_AudioNext, spawn, {.v = nextplay}},
 	{0,                             XF86XK_AudioPrev, spawn, {.v = prevplay}},
 	{0,                             XF86XK_AudioMute, spawn, {.v = mute}},
-	{ MODKEY|ShiftMask,             XK_slash,         swapwindow, {0}},
+	{ MODKEY|ShiftMask,             XK_n,      swapwindow,     {0}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
