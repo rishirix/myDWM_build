9f02aae3b443426d87f87166cfb8b208c15f028e
Rishi <rishibhargawa47@gmail.com>
2025-10-22 15:33:27 +0530

    nothing much

--- config.h
+++ config.h
@@ -2,19 +2,19 @@
 
 #include "movestack.c"
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const unsigned int gappx     = 4;
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Liberation Sans:size=6" };
-static const char dmenufont[]       = "Liberation Sans:size=6";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "Free Sans:style=Bold:size=6" };
+static const char dmenufont[]       = "Free Sans:style=Bold:size=6";
+static const char col_gray1[]       = "#111111";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-i","-p","Seach:",NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *upbrightness[] = {"xbacklight","-inc","10",NULL};
 static const char *downbrightness[] = {"xbacklight","-dec","10",NULL};
